6d71d9a2de4c117708716d7bdfd0d9483466b17a
SankalpPimpalkar <pimpalkarsankalp8@gmail.com>
2026-01-21 16:05:33 +0530

    Updated Connection DB for vercel Deployment

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,11 +1,25 @@
 import mongoose from "mongoose";
 import { MONGO_URI, MONGODB_NAME } from "./env.config.js";
 
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
 export async function dbconnect() {
     try {
-        const response = await mongoose.connect(MONGO_URI, { dbName: MONGODB_NAME })
-        console.log("☘️ Mongodb connected", response.connection.host)
-        return response.connection
+        if (cached.conn) {
+            return cached.conn;
+        }
+
+        if (!cached.promise) {
+            cached.promise = await mongoose.connect(MONGO_URI, { dbName: MONGODB_NAME, bufferCommands: false })
+            console.log("☘️ Mongodb connected", cached.promise.connection.host)
+        }
+
+        cached.conn = await cached.promise;
+        return cached.conn;
 
     } catch (error) {
         console.log("❌ Mongodb failed to connect: ", error)
